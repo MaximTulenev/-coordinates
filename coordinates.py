337a6f0a68b4b50e2d3e1a5ed01b5333bfea544e
MaximTulenev <98822890+MaximTulenev@users.noreply.github.com>
2022-10-05 18:08:48 +0300

    coordinates

---
+++ coordinates.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+
+
+x1=int(input("Введите число"))
+y1=int(input("Введите число"))
+x2=int(input("Введите число"))
+y2=int(input("Введите число"))
+print(sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
